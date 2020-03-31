87900ce9b7510779c6dbecc02c76d619c1d1e367
divyanshu9027 <divyanshu.sharma_iot18@gla.ac.in>
2020-03-31 10:26:02 +0530

    MyMain class of problem2 are successfully runs.

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -7,5 +7,23 @@
 package problem2.main;
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
+import problem1.mybst.MyBinarySearchTree;
+
+import java.util.Scanner;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+        Scanner scanner = new Scanner(System.in);
+        // taking only 7 inputs for simple demonstration with value 1,2,3,4,5,6,7
+        int i = 7;
+        while (i > 0) {
+            tree.add(scanner.nextInt());
+            i--;
+        }
+        tree.preOrder(tree.root);
+        System.out.println();
+        tree.postOrder(tree.root);
+
+    }
 }
