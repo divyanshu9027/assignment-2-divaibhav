4c93301263d5a34b2732e2883a5beb4bf44cbf20
divyanshu9027 <divyanshu.sharma_iot18@gla.ac.in>
2020-03-31 10:23:51 +0530

    MyMain class are completed and it really works.

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,19 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
+import java.util.Scanner;
+
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree tree = new MyBinarySearchTree();
+        Scanner scanner = new Scanner(System.in);
+        // taking only 7 inputs for simple demonstration with value 1,2,3,4,5,6,7
+        int i = 7;
+        while (i > 0) {
+            tree.add(scanner.nextInt());
+            i--;
+        }
+    }
 }
